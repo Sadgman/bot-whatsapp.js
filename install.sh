da03f15d88b1d4b3e92b209f42e7ef62eaf2d843
Sadgman <angel.luis.regalado@hotmail.com>
2024-04-15 11:28:14 -0400

    Actualización del script de instalación

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Autor: @Sadgman
 
 if ! dpkg -l | grep -q nodejs; then
@@ -10,6 +11,17 @@ fi
 
 if ! dpkg -l | grep -q ffmpeg; then
     sudo apt install ffmpeg -y
+fi
+
+if ! dpkg -l | grep -q wget; then
+    sudo apt install wget -y
+fi
+
+if ! dpkg -l | grep -q chromium-browser; then
+    wget https://dl.google.com/linux/direct/chromium-browser_current_amd64.deb
+    chmod +x chromium-browser_current_amd64.deb
+    sudo dpkg -i chromium-browser_current_amd64.deb
+    sudo apt-get install -f
 fi
 
 npm install
