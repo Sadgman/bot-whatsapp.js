51ad78a443dda64fd448a8718adce1d67376a84c
Sadgman <angel.luis.regalado@hotmail.com>
2024-07-18 02:04:41 -0400

    chore: Update index.js to fix casadoContact variable assignment

--- index.js
+++ index.js
@@ -442,7 +442,7 @@ client.on('message_create', async (message) => {
                     } else {
                         casadoContact = await client.getContactById(info.casado + '@c.us');
                         chat.sendMessage(`*Casad@ con:* ${casado}\n*nivel* ${info.nivel}\n*Puntuacion:* ${info.ganadas}\n*Rool:* ${info.roles}\n*Pais:* ${obtenerPais(contact.id.user)}\n*Dinero:* ${info.dinero}\n*Dinero en el banco:* ${info.banco}\n*Total de mensajes enviados:* ${info.mensajes}`, {
-                            mentions: [casadoContact],
+                            mentions: casadoContact,
                             quotedMessageId: quotedMsg.id._serialized
                         });
                     }
