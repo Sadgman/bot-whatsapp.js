e779ca3454f28c6d8b704ce218f5580e6bee5f36
Sadgman <angel.luis.regalado@hotmail.com>
2024-08-07 15:09:44 -0400

    Refactor la inicialización de la base de datos para permitir lectura y escritura

--- utils/base.js
+++ utils/base.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./data.db', (err) => {
+let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
         console.error(err.message);
     }
