371cd6627ce109062e289f3be605a0ecf0892095
Sadgman <angel.luis.regalado@hotmail.com>
2024-08-08 11:32:02 -0400

    Refactor la ruta de la base de datos para utilizar una ubicación absoluta

--- utils/base.js
+++ utils/base.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('/home/sadgman/data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+let db = new sqlite3.Database('/home/sadgman/b/data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
         console.error(err.message);
     }
