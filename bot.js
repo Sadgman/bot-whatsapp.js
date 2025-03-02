0f13161a0a54831dc35d6b3a3408208341954878
Sadgman <angel.luis.regalado@hotmail.com>
2025-03-02 13:49:30 -0400

    Actualizar lógica de verificación de estado de baneo en grupos para usar el mensaje completo

--- bot.js
+++ bot.js
@@ -412,9 +412,8 @@ class AlastorBot {
                         }
                     }
                 }
-                let p = message.body.split(' ');
-                p = p.length > 2 ? p[1] : 'no baneado';
-                if (chat.isGroup && !(await watchBot(chat.id._serialized)) || !db_client || !(watchBan(chat.id._serialized, p))) {
+ 
+                if (chat.isGroup && !(await watchBot(chat.id._serialized)) || !db_client || !(await watchBan(chat.id._serialized, message.body.toLocaleLowerCase()))) {
                     return;
                 }
                 if(chat.isGroup && participantes(numero_cliente)){   
